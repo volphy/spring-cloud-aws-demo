1da7fbcefebf641bfc17ed0154ec69336a8f576b
Krzysztof Wilk <chris.m.wilk@gmail.com>
2017-02-21 09:24:02 +0100

    Implemented updateFile() method (HTTP PUT -> /demo/image/{id})

--- src/main/java/com/krzysztofwilk/spring/cloud/demo/images/ImagesController.java
+++ src/main/java/com/krzysztofwilk/spring/cloud/demo/images/ImagesController.java
@@ -7,17 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -34,14 +40,19 @@ public class ImagesController {
 
     private static final String S3_BUCKET_NAME = "s3BucketName";
 
+    private static final String S3_PROTOCOL_PREFIX = "s3://";
+
     private final ResourcePatternResolver resourcePatternResolver;
+
+    private final ResourceLoader resourceLoader;
 
     private final String s3BucketName;
 
     @Autowired
-    public ImagesController(ResourcePatternResolver resourcePatternResolver) {
+    public ImagesController(ResourcePatternResolver resourcePatternResolver, ResourceLoader resourceLoader) {
 
         this.resourcePatternResolver = resourcePatternResolver;
+        this.resourceLoader = resourceLoader;
 
         if (System.getProperty(S3_BUCKET_NAME) == null) {
             throw new MissingResourceException("Missing S3 Bucket Name", "String", S3_BUCKET_NAME);
@@ -52,7 +63,7 @@ public class ImagesController {
 
     @RequestMapping(method = RequestMethod.GET, value = "images", produces = "application/json")
     public ResponseEntity getFiles() throws IOException {
-        Resource[] allImagesInBucket =  this.resourcePatternResolver.getResources("s3://" + s3BucketName + "/**/*.jpg");
+        Resource[] allImagesInBucket =  this.resourcePatternResolver.getResources(S3_PROTOCOL_PREFIX + s3BucketName + "/**/*.jpg");
 
         AtomicInteger index = new AtomicInteger(1);
         List<FileEntry> files = Arrays.stream(allImagesInBucket)
@@ -68,7 +79,7 @@ public class ImagesController {
     @RequestMapping(method = RequestMethod.GET, value = "image/{id}", produces = "application/octet-stream")
     public ResponseEntity getFile(@PathVariable int id, HttpServletResponse response) throws IOException {
 
-        Resource[] allImagesInBucket =  this.resourcePatternResolver.getResources("s3://" + s3BucketName + "/**/*.jpg");
+        Resource[] allImagesInBucket =  this.resourcePatternResolver.getResources(S3_PROTOCOL_PREFIX + s3BucketName + "/**/*.jpg");
 
         Resource image = Arrays.stream(allImagesInBucket)
                 .sorted(Comparator.comparing(Resource::getFilename))
@@ -84,10 +95,26 @@ public class ImagesController {
             throw e;
         }
 
-        response.addHeader("Content-Disposition", "attachment; filename=" + image.getFilename());
+        response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + image.getFilename());
 
         InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
 
         return new ResponseEntity<>(inputStreamResource, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "image/{id}")
+    public ResponseEntity updateFile(@PathVariable int id, @RequestParam MultipartFile file) throws IOException {
+
+        Resource resource = this.resourceLoader.getResource(S3_PROTOCOL_PREFIX
+                + s3BucketName
+                + "/"
+                + file.getOriginalFilename());
+
+        WritableResource writableResource = (WritableResource) resource;
+        try (OutputStream outputStream = writableResource.getOutputStream()) {
+            outputStream.write(file.getBytes());
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
