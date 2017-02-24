791eeb505a055f77c1d18455845780f3afd1a7be
Krzysztof Wilk <krzysztof.wilk@solidbrain.com>
2017-02-24 14:48:19 +0100

    Fixed updateFile implementation (RFC 2616 states that multipart request should always be HTTP POST)

--- src/main/java/com/krzysztofwilk/spring/cloud/demo/images/ImagesController.java
+++ src/main/java/com/krzysztofwilk/spring/cloud/demo/images/ImagesController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,7 +77,7 @@ class ImagesController {
         return new ResponseEntity<>(inputStreamResource, HttpStatus.OK);
     }
 
-    @PutMapping(value = "image/{id}")
+    @PostMapping(value = "image/{id}")
     ResponseEntity updateFile(@PathVariable int id, @RequestParam MultipartFile file) throws IOException {
         log.info("Updating image no. {} from S3 bucket {}", id, s3BucketName);
 
